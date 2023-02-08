181d896905472334eccf6a35589ab8851a026737
DOH-PXC5303 <philip.crain@doh.wa.gov>
2023-02-08 13:08:22 -0800

    Added package and quotations.RData documentation

---
+++ R/epierror.R
@@ -0,0 +1,7 @@
+#' epierror: Quotes for the Epidemiolgist with an Error
+#'
+#' Easily include moody quotes in your error messages or emails.
+#'
+#' @docType package
+#' @name epierror
+"_PACKAGE"
---
+++ R/quotations.R
@@ -0,0 +1,12 @@
+#' Book Quotes about Errors for Epidemiologists
+#'
+#' A dataset containing edited quotes from classic literature edited to be relevant for epidemiologists running into errors with their code.
+#'
+#' @format A data frame of 38 rows and 3 columns
+#' \describe{
+#'  \item{quote}{Character values with edited book quotes}
+#'  \item{author}{Character values giving the original quotation's author}
+#'  \item{book}{Character values giving the source of the original quotation}
+#' }
+#' @source Many books and the creative minds of Data Integration and Quality Assurance
+"quotations"
