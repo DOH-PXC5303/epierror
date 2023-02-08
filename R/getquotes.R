c03c42b37f5cdc491eb66965025d608f1d052abb
DOH-PXC5303 <philip.crain@doh.wa.gov>
2023-02-08 13:43:01 -0800

    Added description for getquotes

--- R/getquotes.R
+++ R/getquotes.R
@@ -1,3 +1,22 @@
+#' Select some random quotes
+#'
+#' Select `n` number of quotes from \code{quotations} data.frame (included in package) and return the quotes, authors, and book titles as a data.frame.
+#'
+#' @param n Integer or Numeric element: Supplies the number of quotes/rows to be returned. If `n` is larger than the number of rows in \code{quotations}, then the entire df is returned in a random order.
+#'
+#' @return data.frame with randomly selected rows of quote, author, and book.
+#' @export
+#'
+#' @examples
+#' getquotes(1)
+#' getquotes(5)['book']
+#' getquotes(999)$author
+#'
+#' @author Philip Crain <philip.crain@doh.wa.gov>
+#'
+#' @seealso
+#' * \link[epierror]{selectquote} selects quote and returns as a single character element
+#' * \link[epierror]{selectquote.html} selects quote and returns as a single character element with HTML formatting
 getquotes <- function(n) {
 
   if(class(n) != 'integer' & class(n) != 'numeric') {
